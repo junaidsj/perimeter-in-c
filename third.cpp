681daf53791a5562d5487f66643d59b57323e6eb
junaidsj <junaidahmedsj4949@gmail.com>
2024-12-09 16:33:29 +0530

    initial commit

---
+++ third.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l, b, perimeter;
+	cout << "enter the perimeter" << endl;
+	cin >> l >> b;
+	perimeter = 2 * (l + b);
+	cout << "the perimeter is " << perimeter << endl;
+	return 0;
+
+}
